7183a4bebebf84a15fc2bba1a9fb2ff9de0b605f
dotrider <56666461+dotrider@users.noreply.github.com>
2020-05-19 12:56:30 -0700

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-const { PORT_NUM } = process.env;
+const { PORT_NUM, CONNECTION_DB} = process.env;
 const app = express();
 
-
+//Middleware
 app.use(express.json());
+
+//Connecting to DB
+mongoose.connect(CONNECTION_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex:true
+}, () => console.log('Connected to DB'))
 
 app.listen(PORT_NUM, () => console.log(`server running wild on ${PORT_NUM}`))
 
